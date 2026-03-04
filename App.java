2e7ce31afb6521cd0afae82bf57456ed291abaad
Mahesh1-code141 <maheshbabuya@gmail.com>
2026-03-04 13:17:40 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello, Maven");
     }
 
 }
+
